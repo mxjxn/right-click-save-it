8417784a37a0bb29d3e9b71bdbae65221d03e239
MXJXN <modulations@gmail.com>
2023-12-27 22:30:53 -0500

    handle ipfs links

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import './style.css'
-import { ethers } from 'ethers';
-import axios from 'axios';
+import "./style.css";
+import { ethers } from "ethers";
+import axios from "axios";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
 <div>
     <h1 class="text-2xl font-bold mb-4">Fetch Token Image</h1>
     <form id="tokenForm" class="p-6 flex flex-col gap-4 w-full max-w-md mx-auto">
@@ -13,51 +13,66 @@ document.querySelector('#app').innerHTML = `
     </form>
     <div class="flex justify-center items-center" id="imageContainer"></div>
 </div>
-`
+`;
 
 window.fetchTokenImage = async (event) => {
-    event.preventDefault();
-    try {
-        const contractAddress = document.getElementById('contractAddress').value;
-        const tokenId = document.getElementById('tokenId').value;
-        const chainId = document.getElementById('chainId').value || 1;
-        console.log(`fart`);
-        if (!window.ethereum) {
-            throw new Error('Ethereum provider not found');
-        }
-
-        console.log(`butt`);
-        await window.ethereum.request({ method: 'eth_requestAccounts' });
-        if (!window.ethereum) {
-            console.error('Please use an Ethereum-enabled browser or enable the Ethereum extension.');
-            return;
-        }
-        window.ethers = ethers;
-        console.log({
-            ethers,
-            providers: ethers.providers,
-            ethereum: window.ethereum,
-        })
-        const provider = new ethers.BrowserProvider(window.ethereum);
-
-        console.log(`contractAddress: ${contractAddress}\ntokenId: ${tokenId}\nchainId: ${chainId}\nprovider: ${provider}`);
-        const contract = new ethers.Contract(contractAddress, ['function tokenURI(uint256 tokenId) view returns (string)'], provider);
-
-        const tokenURI = await contract.tokenURI(parseInt(tokenId));
-        console.log(`Token URI: ${tokenURI}`);
-
-        const metadataResponse = await axios.get(tokenURI);
-        const metadata = metadataResponse.data;
-        console.log(`Metadata: `, metadata);
-
-        const imageURI = metadata.image || metadata.image_url;
-        if (!imageURI) {
-            throw new Error('Image URI not found in metadata');
-        }
-
-        const imageContainer = document.getElementById('imageContainer');
-        imageContainer.innerHTML = `<img src="${imageURI}" class="w-1/3 mx-auto" alt="Token Image">`;
-    } catch (error) {
-        console.error(`Error fetching token image: ${error.message}`);
+  event.preventDefault();
+  try {
+    const contractAddress = document.getElementById("contractAddress").value;
+    const tokenId = document.getElementById("tokenId").value;
+    const chainId = document.getElementById("chainId").value || 1;
+    console.log(`fart`);
+    if (!window.ethereum) {
+      throw new Error("Ethereum provider not found");
     }
-}
+
+    console.log(`butt`);
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+    if (!window.ethereum) {
+      console.error(
+        "Please use an Ethereum-enabled browser or enable the Ethereum extension."
+      );
+      return;
+    }
+    window.ethers = ethers;
+    console.log({
+      ethers,
+      providers: ethers.providers,
+      ethereum: window.ethereum,
+    });
+    const provider = new ethers.BrowserProvider(window.ethereum);
+
+    console.log(
+      `contractAddress: ${contractAddress}\ntokenId: ${tokenId}\nchainId: ${chainId}\nprovider: ${provider}`
+    );
+    const contract = new ethers.Contract(
+      contractAddress,
+      ["function tokenURI(uint256 tokenId) view returns (string)"],
+      provider
+    );
+
+    const tokenURI = await contract.tokenURI(parseInt(tokenId));
+    console.log(`Token URI: ${tokenURI}`);
+
+    // After fetching tokenURI
+    if (tokenURI.startsWith("ipfs://")) {
+      tokenURI = tokenURI.replace("ipfs://", "https://ipfs.io/ipfs/");
+    }
+    const metadataResponse = await axios.get(tokenURI);
+    const metadata = metadataResponse.data;
+    console.log(`Metadata: `, metadata);
+
+    const imageURI = metadata.image || metadata.image_url;
+    if (!imageURI) {
+      throw new Error("Image URI not found in metadata");
+    }
+    if (imageURI.startsWith("ipfs://")) {
+      imageURI = imageURI.replace("ipfs://", "https://ipfs.io/ipfs/");
+    }
+
+    const imageContainer = document.getElementById("imageContainer");
+    imageContainer.innerHTML = `<img src="${imageURI}" class="w-1/3 mx-auto" alt="Token Image">`;
+  } catch (error) {
+    console.error(`Error fetching token image: ${error.message}`);
+  }
+};
